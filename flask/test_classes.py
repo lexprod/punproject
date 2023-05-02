b2d230b07a145ba0ee82b51cf7518a3668832305
Alex Colby <AlexWColby@gmail.com>
2023-05-01 18:58:18 -0700

    basic class tests

---
+++ flask/test_classes.py
@@ -0,0 +1,17 @@
+from punproj.src.models import User, Pun, Category, puns_categories_table, ratings_table, db
+from punproj.src import create_app
+import pytest
+
+def test_user_init():
+    u1 = User('user', 'pw1')
+    assert u1.user_name == 'user'
+    assert u1.password == 'pw1'
+
+def test_pun_init():
+    p1 = Pun('pun', 1234)
+    assert p1.pun_text == 'pun'
+    assert p1.user_id == 1234
+
+def test_category_init():
+    c1 = Category('category')
+    assert c1.category_name == 'category'
